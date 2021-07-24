a416aedabd4d0f35066e13dcde5ab5b4a73c6df7
Vlad <testerreality@gmail.com>
2021-07-24 13:54:06 +0300

    Инициализация проект
    Создали проект при помощи maven

---
+++ src/main/java/VueSpringProj/Simplegram/SimplegramApplication.java
@@ -0,0 +1,13 @@
+package VueSpringProj.Simplegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VueSpringProj/Simplegram/SimplegramApplicationTests.java
@@ -0,0 +1,13 @@
+package VueSpringProj.Simplegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
