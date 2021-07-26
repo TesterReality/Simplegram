6c9cd0daac3915d54b05d7308420c541da946b3d
Vlad <testerreality@gmail.com>
2021-07-26 15:47:17 +0300

    Начало работы над регистрацией
    В Vue добавлен axios. В Spring начата работа над регистрацией и авторизацией. Добавлены новые зависимости в pom.xml (security, postgresql, jsonwebtoken и validation)

---
+++ src/main/java/VueSpringProj/Simplegram/payload/request/SignupRequest.java
@@ -0,0 +1,56 @@
+package VueSpringProj.Simplegram.payload.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String login;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    private Set<String> role;
+
+    //нужно как-то добавить файл изображения
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRole() {
+        return this.role;
+    }
+
+    public void setRole(Set<String> role) {
+        this.role = role;
+    }
+}
